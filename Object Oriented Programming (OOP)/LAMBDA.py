b2f68db781cc744e8780de63709a36169842e3fc
Michal0ss <122998770+Michal0ss@users.noreply.github.com>
2023-03-11 20:10:53 +0100

    Add files via upload

--- Object Oriented Programming (OOP)/LAMBDA.py
+++ Object Oriented Programming (OOP)/LAMBDA.py
@@ -14,7 +14,7 @@
 double = lambda x: x * 2
 multiply = lambda x, y: x * y
 add = lambda x, y, z: x + y + z
-full_name = lambda first_name, last_name: first_name+" "+last_name
-age_check = lambda age: True if age >=18 else False
-x=int(input("age: "))
+full_name = lambda first_name, last_name: first_name + " " + last_name
+age_check = lambda age: True if age >= 18 else False
+x = int(input("age: "))
 print(age_check(x))
